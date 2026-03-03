4048372892cb5c34f774e473e67722a642e7b2cc
Kei <mikaagang@gmail.com>
2026-03-03 16:42:18 +0800

    fix double path

--- src/login.jsx
+++ src/login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./login.css";
-const API_BASE = "https://weircheve.pythonanywhere.com";
+const API_BASE = "https://weircheve.pythonanywhere.com/dispatch";
 
 const Login = () => {
   const navigate = useNavigate();
