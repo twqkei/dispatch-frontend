c1faad3e32ec48aa2b11ea5fb7bb54d53aaf55c0
Kei <mikaagang@gmail.com>
2026-03-12 09:38:54 +0800

    use remove

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState, useCallback, use } from "react";
+import { useEffect, useMemo, useState, useCallback} from "react";
 import { useNavigate } from "react-router-dom";
 import CalendarComponent from "./calendar";
 import "react-calendar/dist/Calendar.css";
